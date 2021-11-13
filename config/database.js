69e40eb61daf01a08613d837320f399f2bbcaa90
tunng0355 <84296530+tunng0355@users.noreply.github.com>
2021-11-13 15:09:48 +0700

    i
    i

---
+++ config/database.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+	'url': 'mongodb://127.0.0.1:27017',
+	'options': {
+		//'user':   'kvcpro',
+		//'pass':   'Minhhoang2009@',
+		'dbName': 'RVIP', // red
+		'useNewUrlParser': true,
+		'useUnifiedTopology': true,
+		//'autoIndex':       false,
+	},
+};
